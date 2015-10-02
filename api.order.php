68d712b45091f79a8df5a3d39c689df89ce49600
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2015-10-02 14:30:47 +0700

    Development 0.3.3
    - Remove mailer folder.

--- api.order.php
+++ api.order.php
@@ -73,20 +73,49 @@ if($_POST['calling'] != ''){
 							$message 		= file_get_contents('template/email/transfer.html');
 							$message 		= str_replace('%order_id%', $order->id, $message);
 							$message 		= str_replace('%payment%', number_format($order->summary_payments,2), $message);
+							$message 		= str_replace('%expire_date%', $order->expire_time_thai_format, $message);
+							$message 		= str_replace('%expire_count%', $order->expire_time_datediff, $message);
+							// $message 		= str_replace('%bank_list%',, $message);
 							$mail->Body    	= $message;
 							$mail->AltBody 	= 'This is the body in plain text for non-HTML mail clients';
 
-							// Email Sending
 							if(!$mail->send())
 								$email_send = $mail->ErrorInfo;
 							else
 								$email_send = "Message has been sent";
 						}
-						else if($_POST['order_action'] == "TransferRequest"){}
+						else if($_POST['order_action'] == "TransferRequest"){
+							// Sending to Customer
+							$mail->addAddress('mrjimmy18@gmail.com');
+							$mail->Subject 	= '#'.$order->id.' :: กำลังตรวจสอบหลักฐานการโอนเงิน...';
+							$message 		= file_get_contents('template/email/confirm.html');
+							$message 		= str_replace('%order_id%', $order->id, $message);
+							$message 		= str_replace('%payment%', number_format($order->summary_payments,2), $message);
+							$mail->Body    	= $message;
+							$mail->AltBody 	= 'This is the body in plain text for non-HTML mail clients';
+
+							if(!$mail->send())
+								$email_send = $mail->ErrorInfo;
+							else
+								$email_send = "Message has been sent";
+						}
 						else if($_POST['order_action'] == "TransferAgain"){}
 						else if($_POST['order_action'] == "TransferSuccess"){}
 						else if($_POST['order_action'] == "Shipping"){}
-						else if($_POST['order_action'] == "Complete"){}
+						else if($_POST['order_action'] == "Complete"){
+							// Sending to Customer
+							$mail->addAddress('mrjimmy18@gmail.com');
+							$mail->Subject 	= '#'.$order->id.' :: รับสินค้าเรียบร้อย';
+							$message 		= file_get_contents('template/email/complete.html');
+							$message 		= str_replace('%order_id%', $order->id, $message);
+							$mail->Body    	= $message;
+							$mail->AltBody 	= 'This is the body in plain text for non-HTML mail clients';
+
+							if(!$mail->send())
+								$email_send = $mail->ErrorInfo;
+							else
+								$email_send = "Message has been sent";
+						}
 
 						// Return Message all Process in JSON format.
 						$api->successMessage('#'.$_POST['order_id'].' - '.$_POST['order_action'].' Successed! ('.$email_send.')','','');
