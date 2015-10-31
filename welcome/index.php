87c395ba6f4d7cc8120b4e771e50a7df9e9cd0f0
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2015-10-31 16:12:26 +0700

    Development 0.3.97

---
+++ welcome/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="th" itemscope itemtype="http://schema.org/Blog" prefix="og: http://ogp.me/ns#">
+<head>
+
+<!--[if lt IE 9]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+<![endif]-->
+
+<!-- Meta Tag -->
+<meta charset="utf-8">
+
+<!-- Viewport (Responsive) -->
+<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="user-scalable=no">
+<meta name="viewport" content="initial-scale=1,maximum-scale=1">	
+
+<?php
+//include'favicon.php';
+?>
+
+<title>dotdotdot limited in comming soon...</title>
+
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+</head>
+
+<body class="bg-full-screen">
+<div class="filter">
+	<div class="welcome">
+		<h1>dotdotdot limited</h1> 
+		<p>in comming soon...</p>
+	</div>
+</div>
+</body>
+</html>
